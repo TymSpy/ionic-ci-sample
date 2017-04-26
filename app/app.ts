9adf85f1907d75334387d16e25e6e27718ad4888
Mehdhi <mehdhi@cinergix.com>
2017-04-26 11:49:21 +0530

    cleqaing app.ts

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,6 @@ import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
-
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
