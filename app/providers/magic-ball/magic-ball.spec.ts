c1d57521a2427efa3da9d42c9c452aa2c2de7e40
Mehdhi <mehdhi@cinergix.com>
2017-04-27 16:14:44 +0530

    cleaning test

--- app/providers/magic-ball/magic-ball.spec.ts
+++ app/providers/magic-ball/magic-ball.spec.ts
@@ -1,32 +1,24 @@
-import {beforeEachProviders, beforeEach, it, describe, expect, inject, injectAsync} from '@angular/core/testing';
-import {MagicBall} from './magic-ball';
- 
+import { beforeEachProviders, beforeEach, it, describe, expect, inject, injectAsync } from '@angular/core/testing';
+import { MagicBall } from './magic-ball';
+
 describe('Magic 8 Ball Service', () => {
- 
+
     beforeEachProviders(() => [MagicBall]);
- 
+
     it('should return a non empty array', inject([MagicBall], (magicBall) => {
- 
-            let result = magicBall.getAnswers();
- 
-            expect(Array.isArray(result)).toBeTruthy;
-            expect(result.length).toBeGreaterThan(0);
-        }
-    ));
- 
+        let result = magicBall.getAnswers();
+        expect(Array.isArray(result)).toBeTruthy;
+        expect(result.length).toBeGreaterThan(0);
+    }));
+
     it('should return one random answer as a string', inject([MagicBall], (magicBall) => {
-            expect(typeof magicBall.getRandomAnswer()).toBe('string');
-        }
-    ));
- 
+        expect(typeof magicBall.getRandomAnswer()).toBe('string');
+    }));
+
     it('should have both yes and no available in result set', inject([MagicBall], (magicBall) => {
- 
-            let result = magicBall.getAnswers();
- 
-            expect(result).toContain('Yes');
-            expect(result).toContain('No');
- 
-        }
-    ));
- 
+        let result = magicBall.getAnswers();
+        expect(result).toContain('Yes');
+        expect(result).toContain('No');
+    }));
+
 });
